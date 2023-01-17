c45d960834bf3b46daddc5fb256ff4a7bbe5849c
ju-voro <voronetskajaj@hotmail.com>
2023-01-17 20:38:22 +0100

    component export-import

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Technologies } from './components/Technologies';
+import { Header } from './components/Header';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <ul>
-        <li>HTML/CSS</li>
-        <li>JS</li>
-        <li>ReactJS</li>
-      </ul>
+      <Technologies />
     </div>
   );
 }
 
-const Header = () =>{
-  return (
-    <div>
-      <a href="#">Home</a>
-      <a href="#">News Feed</a>
-      <a href="#">Messages</a>
-    </div>
-  )
-}
+
+
+
 
 export default App;
---
+++ src/components/Technologies.tsx
@@ -0,0 +1,9 @@
+export const Technologies = () => {
+  return (
+    <ul>
+        <li>HTML/CSS</li>
+        <li>JS</li>
+        <li>ReactJS</li>
+      </ul>
+  )
+}
